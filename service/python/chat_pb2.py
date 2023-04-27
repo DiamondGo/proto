1d0c08c0ca6b84db35b1de91e655faf3fcc5c8b4
Ken Xie <kexie@arista.com>
2023-04-27 08:52:50 -0700

    add non stream rpc

--- service/python/chat_pb2.py
+++ service/python/chat_pb2.py
@@ -3,6 +3,7 @@
 # source: chat.proto
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
@@ -13,177 +14,13 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor.FileDescriptor(
-  name='chat.proto',
-  package='service',
-  syntax='proto3',
-  serialized_options=b'Z\nservice/go',
-  create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\nchat.proto\x12\x07service\"\x80\x01\n\x07Message\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1b\n\x04\x63hat\x18\x02 \x01(\x0b\x32\r.service.Chat\x12\x1d\n\x06sender\x18\x03 \x01(\x0b\x32\r.service.User\x12\x0c\n\x04text\x18\x04 \x01(\t\x12\x0c\n\x04\x64\x61ta\x18\x05 \x01(\x0c\x12\x11\n\treplyToId\x18\x06 \x01(\t\"2\n\x04\x43hat\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1e\n\x07members\x18\x02 \x03(\x0b\x32\r.service.User\"9\n\x04User\x12\n\n\x02id\x18\x01 \x01(\t\x12\x12\n\nfirst_name\x18\x02 \x01(\t\x12\x11\n\tuser_name\x18\x03 \x01(\t2B\n\x08\x43hatting\x12\x36\n\nChatStream\x12\x10.service.Message\x1a\x10.service.Message\"\x00(\x01\x30\x01\x42\x0cZ\nservice/gob\x06proto3'
-)
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nchat.proto\x12\x07service\"\x80\x01\n\x07Message\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1b\n\x04\x63hat\x18\x02 \x01(\x0b\x32\r.service.Chat\x12\x1d\n\x06sender\x18\x03 \x01(\x0b\x32\r.service.User\x12\x0c\n\x04text\x18\x04 \x01(\t\x12\x0c\n\x04\x64\x61ta\x18\x05 \x01(\x0c\x12\x11\n\treplyToId\x18\x06 \x01(\t\"2\n\x04\x43hat\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1e\n\x07members\x18\x02 \x03(\x0b\x32\r.service.User\"9\n\x04User\x12\n\n\x02id\x18\x01 \x01(\t\x12\x12\n\nfirst_name\x18\x02 \x01(\t\x12\x11\n\tuser_name\x18\x03 \x01(\t2p\n\x08\x43hatting\x12\x36\n\nChatStream\x12\x10.service.Message\x1a\x10.service.Message\"\x00(\x01\x30\x01\x12,\n\x04\x43hat\x12\x10.service.Message\x1a\x10.service.Message\"\x00\x42\x0cZ\nservice/gob\x06proto3')
 
 
 
-
-_MESSAGE = _descriptor.Descriptor(
-  name='Message',
-  full_name='service.Message',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='id', full_name='service.Message.id', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='chat', full_name='service.Message.chat', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='sender', full_name='service.Message.sender', index=2,
-      number=3, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='text', full_name='service.Message.text', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='data', full_name='service.Message.data', index=4,
-      number=5, type=12, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"",
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='replyToId', full_name='service.Message.replyToId', index=5,
-      number=6, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=24,
-  serialized_end=152,
-)
-
-
-_CHAT = _descriptor.Descriptor(
-  name='Chat',
-  full_name='service.Chat',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='id', full_name='service.Chat.id', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='members', full_name='service.Chat.members', index=1,
-      number=2, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=154,
-  serialized_end=204,
-)
-
-
-_USER = _descriptor.Descriptor(
-  name='User',
-  full_name='service.User',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='id', full_name='service.User.id', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='first_name', full_name='service.User.first_name', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='user_name', full_name='service.User.user_name', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=206,
-  serialized_end=263,
-)
-
-_MESSAGE.fields_by_name['chat'].message_type = _CHAT
-_MESSAGE.fields_by_name['sender'].message_type = _USER
-_CHAT.fields_by_name['members'].message_type = _USER
-DESCRIPTOR.message_types_by_name['Message'] = _MESSAGE
-DESCRIPTOR.message_types_by_name['Chat'] = _CHAT
-DESCRIPTOR.message_types_by_name['User'] = _USER
-_sym_db.RegisterFileDescriptor(DESCRIPTOR)
-
+_MESSAGE = DESCRIPTOR.message_types_by_name['Message']
+_CHAT = DESCRIPTOR.message_types_by_name['Chat']
+_USER = DESCRIPTOR.message_types_by_name['User']
 Message = _reflection.GeneratedProtocolMessageType('Message', (_message.Message,), {
   'DESCRIPTOR' : _MESSAGE,
   '__module__' : 'chat_pb2'
@@ -205,32 +42,17 @@ User = _reflection.GeneratedProtocolMessageType('User', (_message.Message,), {
   })
 _sym_db.RegisterMessage(User)
 
+_CHATTING = DESCRIPTOR.services_by_name['Chatting']
+if _descriptor._USE_C_DESCRIPTORS == False:
 
-DESCRIPTOR._options = None
-
-_CHATTING = _descriptor.ServiceDescriptor(
-  name='Chatting',
-  full_name='service.Chatting',
-  file=DESCRIPTOR,
-  index=0,
-  serialized_options=None,
-  create_key=_descriptor._internal_create_key,
-  serialized_start=265,
-  serialized_end=331,
-  methods=[
-  _descriptor.MethodDescriptor(
-    name='ChatStream',
-    full_name='service.Chatting.ChatStream',
-    index=0,
-    containing_service=None,
-    input_type=_MESSAGE,
-    output_type=_MESSAGE,
-    serialized_options=None,
-    create_key=_descriptor._internal_create_key,
-  ),
-])
-_sym_db.RegisterServiceDescriptor(_CHATTING)
-
-DESCRIPTOR.services_by_name['Chatting'] = _CHATTING
-
+  DESCRIPTOR._options = None
+  DESCRIPTOR._serialized_options = b'Z\nservice/go'
+  _MESSAGE._serialized_start=24
+  _MESSAGE._serialized_end=152
+  _CHAT._serialized_start=154
+  _CHAT._serialized_end=204
+  _USER._serialized_start=206
+  _USER._serialized_end=263
+  _CHATTING._serialized_start=265
+  _CHATTING._serialized_end=377
 # @@protoc_insertion_point(module_scope)
--- service/python/chat_pb2_grpc.py
+++ service/python/chat_pb2_grpc.py
@@ -19,6 +19,11 @@ class ChattingStub(object):
                 request_serializer=chat__pb2.Message.SerializeToString,
                 response_deserializer=chat__pb2.Message.FromString,
                 )
+        self.Chat = channel.unary_unary(
+                '/service.Chatting/Chat',
+                request_serializer=chat__pb2.Message.SerializeToString,
+                response_deserializer=chat__pb2.Message.FromString,
+                )
 
 
 class ChattingServicer(object):
@@ -30,11 +35,22 @@ class ChattingServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def Chat(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_ChattingServicer_to_server(servicer, server):
     rpc_method_handlers = {
             'ChatStream': grpc.stream_stream_rpc_method_handler(
                     servicer.ChatStream,
+                    request_deserializer=chat__pb2.Message.FromString,
+                    response_serializer=chat__pb2.Message.SerializeToString,
+            ),
+            'Chat': grpc.unary_unary_rpc_method_handler(
+                    servicer.Chat,
                     request_deserializer=chat__pb2.Message.FromString,
                     response_serializer=chat__pb2.Message.SerializeToString,
             ),
@@ -60,6 +76,23 @@ class Chatting(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.stream_stream(request_iterator, target, '/service.Chatting/ChatStream',
+            chat__pb2.Message.SerializeToString,
+            chat__pb2.Message.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def Chat(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/service.Chatting/Chat',
             chat__pb2.Message.SerializeToString,
             chat__pb2.Message.FromString,
             options, channel_credentials,
